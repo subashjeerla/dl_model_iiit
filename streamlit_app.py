1b60a6ae92870ce5c00c1574f72a44e564cd04a3
subashjeerla <229x1a05h5@gprec.ac.in>
2025-10-12 19:50:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,5 @@
-
+import streamlit as st
+st.title("🎈  dl prediction model")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
