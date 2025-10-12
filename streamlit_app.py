dcfd0d941cee163b46e36b0afbc525333dac5106
subashjeerla <229x1a05h5@gprec.ac.in>
2025-10-12 20:03:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 st.title("🎈  dl prediction model")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+df = pd.read_csv("https://raw.githubusercontent.com/subashjeerla/dl_model_iiit/refs/heads/main/real_2015")
+df
